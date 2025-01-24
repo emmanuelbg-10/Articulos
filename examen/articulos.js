8457214506825361baba9bca2efbb6eb0e937b26
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-01-24 13:35:53 +0000

    1

--- examen/articulos.js
+++ examen/articulos.js
@@ -1,73 +1,81 @@
 const bodyArticulos = document.querySelector('#articulos tbody');
 const form = document.getElementById('form_crear');
+
 let categorias;
 
 async function cargarCategorias() {
   try {
-  const response = await fetch('http://localhost:3000/api/categoria');
-  if(!response.ok) {
-    throw new Error("Fallo http: " +response.status);
-  }
-  cargarArticulos();
-  categorias = await response.json();
-} catch (e) {
-  console.error('Error en el fetch de categorias '+ e);
+    const response = await fetch('http://localhost:3000/api/categoria');
+    if (!response.ok) {
+      throw new Error('Fallo http: ' + response.status);
+    }
+
+    categorias = await response.json();
+    cargarArticulos();
+  } catch (e) {
+    console.error('Error en el fetch de categorias: ' + e);
   }
 }
-
 
 async function cargarArticulos() {
   try {
     const response = await fetch('http://localhost:3000/api/articulo');
 
-    if(!response.ok) {
-      throw new Error("Fallo http");
-      
+    if (!response.ok) {
+      throw new Error('Fallo http');
     }
     const articulos = await response.json();
 
-articulos.forEach(articulo => {
-  renderArticulo(articulo);
-});
- } catch (e) {
-  console.error('Error en el fetch '+ e);
+    articulos.forEach(articulo => {
+      renderArticulo(articulo);
+    });
+  } catch (e) {
+    console.error('Error en el fetch: ' + e);
   }
 }
 
 function renderArticulo(articulo) {
-  const tr = document.createElement('tr');
+  let categoriasSaved = [];
+  if (localStorage.categorias) {
+    categoriasSaved = JSON.parse(localStorage.categorias);
+  }
 
-  let td = document.createElement('td');
-  td.textContent = articulo.id;
-  tr.append(td);
+  if (categoriasSaved.includes(articulo.id_categoria.toString())) {
+    const tr = document.createElement('tr');
+    let td = document.createElement('td');
+    td.textContent = articulo.id;
+    tr.append(td);
 
-  td = document.createElement('td');
-  // td.textContent = articulo.id_categoria;
-  td.textContent = categorias.filter(c => c.id== articulo.id_categoria)[0].nombre;
-  tr.append(td);
+    td = document.createElement('td');
+    /*Esto es para que se vea el nombre de la categoria y no el id*/
+    td.textContent = categorias.filter(c => c.id == articulo.id_categoria)[0].nombre
+    tr.append(td);
 
-  td = document.createElement('td');
-  td.textContent = articulo.usuario;
-  tr.append(td);
+    td = document.createElement('td');
+    td.textContent = articulo.usuario;
+    tr.append(td);
 
-  td = document.createElement('td');
-  td.textContent = articulo.fecha;
-  tr.append(td);
+    td = document.createElement('td');
+    td.textContent = articulo.fecha;
+    tr.append(td);
 
-  td = document.createElement('td');
-  td.textContent = articulo.titulo;
-  tr.append(td);
+    td = document.createElement('td');
+    td.textContent = articulo.titulo;
+    tr.append(td);
 
-  bodyArticulos.append(tr);
+    bodyArticulos.append(tr);
+  }
 }
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
+
   const articulo = {
-    "id_categoria": form.categoria.value,
+    "id_categoria": form.id_categoria.value,
     "usuario": form.usuario.value,
     "titulo": form.titulo.value,
-    "cuerpo": form.cuerpo.value,
+    "cuerpo": form.cuerpo.value
   }
+
   const options = {
     method: 'post',
     headers: {
@@ -77,14 +85,15 @@ form.addEventListener('submit', async (e) => {
   }
   console.log(options);
   try {
-  const response = await fetch('http://localhost:3000/api/articulo', options)
-  if(!response.ok) {
-    throw new Error("Fallo http en el crear: "+ response.status);
+    const response = await fetch('http://localhost:3000/api/articulo', options);
+    if (!response.ok) {
+      throw new Error('Fallo http en el crear: ' + response.status);
+    }
+    const articulo = await response.json();
+    renderArticulo(articulo);
+  } catch (e) {
+    console.error('Error en el fetch de crear: ' + e);
   }
-  const articulo = await response.json();
-  renderArticulo(articulo);
-} catch (e) {
-  console.error('Error en el fetch de crear: '+ e)
-}
-});
+})
+
 cargarCategorias();
--- examen/categorias.js
+++ examen/categorias.js
@@ -1,16 +1,34 @@
 const divCategories = document.getElementById('categorias')
 
 fetch('http://localhost:3000/api/categoria')
-.then(res => res.json())
-.then(categories => categories.forEach(c => {
-  renderInputCategory(c);
-}));
+  .then(res => res.json())
+  .then(categories => categories.forEach(c => {
+    renderInputCategory(c);
+  }));
 
-function renderInputCategory(category){
+function renderInputCategory(category) {
+  let checked = '';
+  console.log(category.id.toString());
+  if (localStorage.categorias) {
+    const categoriasChecked = JSON.parse(localStorage.categorias);
+
+    checked = categoriasChecked.includes(category.id.toString()) ? 'checked' : '';
+  }
+
   divCategories.insertAdjacentHTML('beforeend', `
     <p>
-      <input type="checkbox" name="categories[]" id="op${ category.id }} value=${ category.id }}"
-      <label for="op${ category.id }">${ category.nombre }</label>
+      <input type="checkbox" name="categories[]" id="op${category.id}" value="${category.id}" ${checked}
+      <label for="op${category.id}">${category.nombre}</label>
     </p>
     `)
 }
+
+const actualizar = document.querySelector('[name = actualizar]');
+actualizar.addEventListener("click", () => {
+  const categorias = document.querySelectorAll('div input');
+  const categoriasChecked = Array.from(categorias)
+    .filter(categoria => categoria.checked)  // Filtra solo las categorías que están seleccionadas
+    .map(categoria => categoria.value);  // Mapea solo los valores de las categorías seleccionadas
+
+  localStorage.setItem('categorias', JSON.stringify(categoriasChecked));  // Usamos setItem para almacenar en localStorage
+})
